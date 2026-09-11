17b68c8ab9b2d10a9d77e2bfbfe4b88189a67093
Khania Puji Auliya <khaniapujia@gmail.com>
2026-09-11 08:42:16 +0700

    Percobaan 4

--- src/GetterSetterCooperative/KoperasiDemo.java
+++ src/GetterSetterCooperative/KoperasiDemo.java
@@ -1,6 +1,7 @@
 public class KoperasiDemo {
     public static void main(String[] args) {
-        Member anggota1 = new Member();
+        Member anggota1 = new Member("Iwan", "Jalan Mawar");
+        System.out.println("Simpanan " + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
         anggota1.setNama("Iwan Setiawan");
         anggota1.setAlamat("Jalan Sukarno Hatta no 10");
         anggota1.setor(100000);
--- src/GetterSetterCooperative/Member.java
+++ src/GetterSetterCooperative/Member.java
@@ -3,6 +3,12 @@ public class Member {
     public String alamat;
     public float simpanan;
 
+    Member(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.simpanan = 0;
+    }
+
     public void setNama(String nama) {
         this.nama = nama;
     }
